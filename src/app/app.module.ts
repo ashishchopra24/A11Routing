7bca2524f53a692419d6234ae85351b5c5b2308c
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2020-04-14 21:29:49 +0530

    LoadingRoutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './servers/server/server.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {UserComponent} from './users/user/user.component';
+import {ServersComponent} from './servers/servers.component';
+import {UsersComponent} from './users/users.component';
+import {HomeComponent} from './home/home.component';
+import {FormsModule} from '@angular/forms';
+import {ServersService} from './servers/servers.service';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'servers', component: ServersComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
